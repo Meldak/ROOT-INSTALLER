fd64614db7113f6b9b6cbd8d8f63ee457d1d17d3
irving <irving@venus.lns>
2018-11-06 18:14:43 -0600

    Correcciones

--- install-root.sh
+++ install-root.sh
@@ -17,65 +17,64 @@ p_exe) P_EXE=${OPTARG};;
 p_in) P_INCLUDE=${OPTARG};;
 esac
 done
-
+WORKDIR="/opt"
 VERSION=6-14-04
 P_EXE="/usr/bin/python3"
 P_INCLUDE="/usr/include/python3.4m"
 
+################### Download compiled BIN for Python 2 ########################
+#PKG="root_v${VERSION}.Linux-centos7-x86_64-gcc4.8.tar.gz"  
+#REPO="https://root.cern.ch/download/"$PKG                  
+#wget $REPO                                                 
+#echo "----------DESCOMPRIMIENDO ROOT V${VERSION}----------"
+#tar vzxf ./$PKG 
+#sudo mv ./root /opt/                                      
+##################################################################
 
-if [ ! -d ~/ROOT ]; then
+if [ ! -d $WORKDIR/ROOT ]; then
 echo "----------CREANDO CARPETA DE DESCARGA------------"
-mkdir ~/ROOT    
+sudo mkdir $WORKDIR/ROOT    
 else
-echo ">> Listo ~/ROOT"
-cd ~/ROOT
-if [ ! -d ~/ROOT/root ]; then
+echo ">> Listo ${WORKDIR}/ROOT"
+cd $WORKDIR/ROOT
+if [ ! -d $WORKDIR/ROOT/root ]; then
 echo "----------DESCARGANDO ROOT V${VERSION}------------"
-git clone http://github.com/root-project/root.git
+sudo git clone http://github.com/root-project/root.git
 cd root
-git checkout -b v$VERSION v$VERSION  
+sudo git checkout -b v$VERSION v$VERSION  
 fi
 echo ">> Lista descarga ROOT V${VERSION}"
 fi
 
 
-################### Download compiled BIN ########################
-### PKG="root_v${VERSION}.Linux-centos7-x86_64-gcc4.8.tar.gz"  ###
-### REPO="https://root.cern.ch/download/"$PKG                  ###
-### wget $REPO                                                 ###
-### echo "----------DESCOMPRIMIENDO ROOT V${VERSION}----------"###
-### tar vzxf ./$PKG                                            ###
-##################################################################
 
 
-if [ ! -d ~/ROOT/build ]; then
+
+if [ ! -d $WORKDIR/ROOT/build ]; then
 echo "----------CREANDO CARPETA PARA COMPILADO------------"
-mkdir ~/ROOT/build    
+sudo mkdir $WORKDIR/ROOT/build    
 else
-echo ">> Listo ~/ROOT/build"
-cd ~/ROOT/build
+echo ">> Listo ${WORKDIR}/ROOT/build"
+cd $WORKDIR/ROOT/build
 fi
 
-if [ ! -d ~/ROOT/root ]; then
-echo "No se encuentra archivos fuente en ~/ROOT/ "
+if [ ! -d $WORKDIR/ROOT/root ]; then
+echo "No se encuentra archivos fuente en ${WORKDIR}/ROOT/ "
 exit
 else
-echo ">> Listo ~/ROOT/root"
+echo ">> Listo ${WORKDIR}/ROOT/root"
 echo "----------CONFIGURANDO------------"
-cmake3 ../root -DPYTHON_EXECUTABLE=$P_EXE -DPYTHON_INCLUDE_DIR=$P_INCLUDE -Dpython3=ON
+sudo cmake3 ../root -DPYTHON_EXECUTABLE=$P_EXE -DPYTHON_INCLUDE_DIR=$P_INCLUDE -Dpython3=ON
 fi
-
-
 
 echo "----------COMPILANDO------------"
 make -j8
 
-sudo cp ~/ROOT/build /opt/root
 
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "   No olvides ejecutar el comando     "
-echo "  source /opt/root/bin/thisroot.sh    "
+echo "  source /opt/ROOT/build/bin/thisroot.sh    "
 echo " O agregarlo a tu configuracion bash  "
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
